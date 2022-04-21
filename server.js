8c76393bd5b52271a5af20866572704e25c9125a
AdamFinder <fin19011@byui.edu>
2022-04-20 17:32:16 -0700

    Week 1 Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use('/', require('./routes'))
+
+// app.get('/', (req, res) => {
+//     res.send('Justin Murn')
+// });
+
+app.listen(port, () => {
+    console.log('Running on port ${port}')
+});
